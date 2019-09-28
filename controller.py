5f471c160adbc203bb7a26524791021f246f003e
maaya <huki.j37@gmail.com>
2019-09-28 22:14:58 +0900

    だいたいこんな感じの回答が出てくるであろう

--- controller.py
+++ controller.py
@@ -1,6 +1,7 @@
 from flask import Flask
-# from flask import request
 import flask
+import glob
+import os
 app = Flask(__name__)
 
 
@@ -11,6 +12,17 @@ def show_index():
     )
 
 # ★課題1★ work-details.htmlを表示させるコントローラを作成してください
+@app.route('/about')
+def show_about_us():
+    image_list = glob.glob(os.path.join('.',  'static', 'images', 'caravan', '*'))
+    images = []
+    for i in image_list:
+        images.append(os.path.basename(i))
+
+    return flask.render_template(
+        'works-details.html',
+        images=images
+    )
 
 
 if __name__ == "__main__":
